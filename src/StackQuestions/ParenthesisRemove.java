86eaf671a65df453fdc9afe543dcac7de425a3f9
=Ayushi Saxena <saxenaayu2106@gmail.com>
2023-01-10 12:11:30 +0530

    adding 1 more

--- src/StackQuestions/ParenthesisRemove.java
+++ src/StackQuestions/ParenthesisRemove.java
@@ -1,5 +1,32 @@
 package StackQuestions;
 
+import java.util.Stack;
+
 public class ParenthesisRemove {
 
+	public static String removeOuterParenthesis(String s) {
+		        int count = 0;
+		        StringBuilder str = new StringBuilder();
+
+		        for(int i = 0; i<s.length(); i++){
+		            if( s.charAt(i) == '('){
+		                count++;
+		                if(count>=2) str.append('(');
+		            }else {
+		                if(count>=2) str.append(')');
+		                count--;
+		            }
+		        }
+
+		        return str.toString();
+		        }
+	
+	
+	public static void main(String args[]) {
+		String str = "(()())(())";
+		String ans = removeOuterParenthesis(str);
+		System.out.println(ans);
+		
+	}
+	
 }
