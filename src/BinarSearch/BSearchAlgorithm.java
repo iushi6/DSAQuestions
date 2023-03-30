0365bdb0df4de34b228e63cb3100fa93828c4a90
=Ayushi Saxena <saxenaayu2106@gmail.com>
2023-03-30 08:10:22 +0530

    Adding BinarySearch Algo

---
+++ src/BinarSearch/BSearchAlgorithm.java
@@ -0,0 +1,37 @@
+package BinarSearch;
+import java.util.Scanner;
+
+public class BSearchAlgorithm {
+//Sorted Array is Given -- >
+	
+	static int searchIndexOfElement(int[] a, int x) {
+		int start = 0;
+		int end = a.length-1;
+		
+		while(start<=end){
+			int mid = start - (start - end)/2;
+			
+			if(a[mid]==x) {
+				return mid;
+			}else if(x > a[mid]) {
+				start = mid + 1;
+			}else {
+				end = mid - 1;
+			}
+		}
+		return -1;
+	}
+	
+	public static void main(String args[]) {
+		Scanner sc = new Scanner (System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i =0;i<n;i++) {
+	 arr[i] = sc.nextInt();		
+	}
+		int searchIndex = sc.nextInt();
+	int ans = searchIndexOfElement(arr, searchIndex);	
+	
+	System.out.println("Required Index of Element is : "+ans);
+  }
+}
---
+++ src/TreeQuestions/inorderTraversal.java
@@ -0,0 +1,5 @@
+package TreeQuestions;
+
+public class inorderTraversal {
+
+}
